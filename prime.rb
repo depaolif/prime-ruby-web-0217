83631d6a6581b0e9985bdc7315afee2f548b5b1e
harleqat <harleqat@gmail.com>
2017-01-01 18:04:12 -0500

    tested

--- prime.rb
+++ prime.rb
@@ -1,1 +1,17 @@
 # Add  code here!
+def prime?(int)
+	if int <= 1
+		return false
+	elsif int == 2
+		return true
+	end
+	array = (2...int).to_a
+	array.each do |x|
+		if int % x == 0
+			return false
+		end
+	end
+	return true
+end
+
+prime?(101013)
